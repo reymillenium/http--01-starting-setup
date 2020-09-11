0ebc5443f34c007e48a77ad44ecb2f3ba0bed1ce
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-11 06:11:46 -0400

    Mod gitignore

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,14 +7,22 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-            console.log(response)
+            this.setState({
+                posts: response.data
+            });
+            // console.log(response)
         });
     }
 
     render() {
+        
+
         return (
             <div>
                 <section className="Posts">
