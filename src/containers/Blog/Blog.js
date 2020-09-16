82b14f44de84e828fd253d4ae2ee90c74707a692
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-16 00:19:00 -0400

    Implemented error catching on the Blog container bu using the catch method

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -24,6 +25,11 @@ class Blog extends Component {
             this.setState({
                 posts: updatedPosts
             });
+        }).catch(error => {
+            // console.log(error);
+            this.setState({
+                error: true
+            });
         });
     }
 
@@ -34,17 +40,20 @@ class Blog extends Component {
     }
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return (
-                <Post
-                    key={post.id}
-                    title={post.title}
-                    content={post.body}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
-            );
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something wen wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        content={post.body}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                );
+            });
+        }
 
         return (
             <div>
