3a470eb8851b4e2dc8cbb79cd5a25b4066201439
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-16 00:58:04 -0400

    Added global handling of requests and responses and its errors on the Index.js file, by using axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(requestConfig => {
+    console.log(requestConfig);
+    // We could also Edit the request configuration before returning the requestConfig (that's the idea behind the interceptor)
+    // And we could also add headers (example: an authorization header), etc
+    return requestConfig;
+}, error => {
+    // Handles any error related to the sending request (example: No internet connection)
+    // Does not handles received responses
+    console.log(error);
+    // We still forward it to our response as I wrote it into the component, where wen can handle it again with the catch method
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(successResponse => {
+    console.log(successResponse);
+
+    // So wen can use it on the 'then' block of the component
+    // We could also manipulate the response here or logging in, etc
+    return successResponse;
+}, error => {
+    // Handles any error related to the received response  (example: )
+    // Does not handles sending requests
+    console.log(error);
+    // We still forward it to our request as I wrote it into the component, where wen can handle it again with the catch method
+    //This makes sense if we have some local task I wanna do, like showing something on the UI...
+    // ... but also globally I want to log it into the log file which I send to a server or something like that
+    return Promise.reject(error);
+});
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
