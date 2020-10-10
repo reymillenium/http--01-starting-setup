5818b9707affeb6f5ada3e1cf0b93a8d3bee484a
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-09 23:21:46 -0400

    Moved all the posts logic from the Blog component into the new Posts component. Addded the Posts.css file.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,60 +3,20 @@ import React, {Component} from 'react';
 import axios from '../../axios';
 
 
-import Post from '../../components/Post/Post';
+// import Post from '../../components/Post/Post';
 // import FullPost from '../FullPost/FullPost';
 // import NewPost from '../NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-        // axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-        axios.get('/posts').then(response => {
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'Max'
-                }
-            });
-            this.setState({
-                posts: updatedPosts
-            });
-        }).catch(error => {
-            // console.log(error);
-            this.setState({
-                error: true
-            });
-        });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId: id
-        });
-    }
+    // state = {
+    //     posts: [],
+    //     selectedPostId: null,
+    //     error: false
+    // }
 
     render() {
-        let posts = <p style={{textAlign: 'center'}}>Something wen wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return (
-                    <Post
-                        key={post.id}
-                        title={post.title}
-                        content={post.body}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}
-                    />
-                );
-            });
-        }
 
         return (
             <div className="Blog">
@@ -68,9 +28,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+
+                <Posts/>
 
                 {/*<section>*/}
                 {/*    <FullPost id={this.state.selectedPostId}/>*/}
