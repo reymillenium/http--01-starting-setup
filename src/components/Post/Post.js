0c09aa2da7da39aa6f0ec9416ea512890441a467
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-11 06:05:56 -0400

    Modified the Post component, temporaly, by adding withRouterf

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,17 +1,22 @@
 import React from 'react';
+// import {withRouter} from "react-router";
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post" onClick={props.clicked}>
-        <h1>{props.title}</h1>
-        <p>
-            {props.content}
-        </p>
-        <div className="Info">
-            <div className="Author">{props.author}</div>
-        </div>
-    </article>
-);
+const post = (props) => {
+    // console.log(props);
+    return (
+        <article className="Post" onClick={props.clicked}>
+            <h1>{props.title}</h1>
+            <p>
+                {props.content}
+            </p>
+            <div className="Info">
+                <div className="Author">{props.author}</div>
+            </div>
+        </article>
+    );
+}
 
 export default post;
+// export default withRouter(post);
